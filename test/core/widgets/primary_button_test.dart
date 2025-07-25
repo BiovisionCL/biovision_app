fa4c6db297ec29d192529f8ba7d1b3cde67771d8
Fernando Fuentes-Peñailillo <pfuentesfernando@gmail.com>
2025-07-25 19:54:38 -0400

    --f=/Users/fernandofuentes/Library/Jupyter/runtime/kernel-v3e1c87204b3b4718a0a93ba5de9ab6ee0306c0392.json

--- test/core/widgets/primary_button_test.dart
+++ test/core/widgets/primary_button_test.dart
@@ -1,12 +1,20 @@
-
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:biovision_app/core/widgets/primary_button.dart';
 
 void main() {
   testWidgets('PrimaryButton renders with given text', (WidgetTester tester) async {
     await tester.pumpWidget(
-      PrimaryButton(text: 'Click Me', onPressed: () {}),
+      MaterialApp(       // <--- Esto provee Directionality y tema Material
+        home: Scaffold(
+          body: PrimaryButton(
+            text: 'Click Me',
+            onPressed: () {},
+          ),
+        ),
+      ),
     );
+
     expect(find.text('Click Me'), findsOneWidget);
   });
 }
