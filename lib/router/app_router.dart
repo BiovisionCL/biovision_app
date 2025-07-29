eb0b64261792ba856df852c1f995df84fe5f9dc3
Fernando Fuentes-Peñailillo <pfuentesfernando@gmail.com>
2025-07-29 17:09:52 -0400

    🧭 Add /quienes-somos route to AppRouter

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../features/home/presentation/home_page.dart';
+import '../features/about/about_page.dart';
 
 class AppRouter {
   static final GoRouter router = GoRouter(
@@ -8,6 +9,10 @@ class AppRouter {
       GoRoute(
         path: '/',
         builder: (context, state) => const HomePage(),
+      ),
+      GoRoute(
+        path: '/quienes-somos',
+        builder: (context, state) => const AboutPage(),
       ),
     ],
   );
