c24fca19792e16475cff20fd6b6f05a5d6376834
Fernando Fuentes-Peñailillo <pfuentesfernando@gmail.com>
2025-07-29 16:02:00 -0400

    🚀 Setup main.dart with GoRouter and AppTheme integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
-import 'features/home/presentation/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'router/app_router.dart';
+import 'core/theme/app_theme.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const BiovisionApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class BiovisionApp extends StatelessWidget {
+  const BiovisionApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Biovision App',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const HomePage(),
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'Biovision',
+      theme: AppTheme.lightTheme,
+      routerConfig: AppRouter.router,
     );
   }
 }
